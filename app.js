35b563d40689a7471f7a0b9362245bcb43892f46
mariesajan <mariesajan@gmail.com>
2016-02-12 16:16:45 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    usernames = [];
+
+
+server.listen((process.env.PORT || 3000), function() {
+    console.log('server is listening to 3000.......');
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+
+    socket.on('chat message', function(data) {
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', function() {
+        //socket.username will be undefined if here is any error with username and then disconnects. So no updation iof usernames should happen
+        if (socket.username != undefined) {
+            var index = usernames.indexOf(socket.username);
+            usernames.splice(index, 1);
+            updateUsernames();
+        }
+    });
+
+    socket.on('new user', function(uname, callback) {
+        if (usernames.indexOf(uname) != -1) {
+            socket.emit('error found', {
+                msg: "Username already taken"
+            });
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = uname;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+
+    function updateUsernames() {
+        io.emit('username', usernames);
+    }
+});
